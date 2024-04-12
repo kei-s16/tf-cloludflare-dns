fa36b5ebeb9709a57c0acbfeba4504f3aeb5d58b
kei-s16 <kei1216s@nifty.com>
2024-04-12 22:58:58 +0900

    descriptionやtypeを追加

--- variables.tf
+++ variables.tf
@@ -1,9 +1,24 @@
 variable "cloudflare_zone_id" {
   sensitive = true
+  type = string
+  description = <<EOT
+    ドメインのゾーンID
+
+    refs: https://developers.cloudflare.com/fundamentals/setup/find-account-and-zone-ids/
+  EOT
 }
 
 variable "cloudflare_account_id" {
   sensitive = true
+  type = string
+  description = <<EOT
+    アカウントID
+
+    refs: https://developers.cloudflare.com/fundamentals/setup/find-account-and-zone-ids/
+  EOT
 }
 
-variable "cloudflare_domain" {}
+variable "cloudflare_domain" {
+  type = string
+  description = "管理するドメイン名"
+}
